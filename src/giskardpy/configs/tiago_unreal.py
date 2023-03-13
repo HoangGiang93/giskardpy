3e1c6ffb329c7ee859388617b82435c45eb4575a
Giang Nguyen <hoanggia@uni-bremen.de>
2023-03-13 13:32:09 +0100

    Update tiago

--- src/giskardpy/configs/tiago_unreal.py
+++ src/giskardpy/configs/tiago_unreal.py
@@ -16,3 +16,19 @@ class TiagoUnreal(Giskard):
                                                 state_topic='/tiago/arm_right_controller/state')
         self.add_base_cmd_velocity(cmd_vel_topic='/tiago/cmd_vel',
                                    track_only_velocity=False)
+
+class TiagoUnrealNoPrefix(Giskard):
+    def __init__(self):
+        self.add_robot_from_parameter_server(
+            joint_state_topics=['/joint_states'])
+        super().__init__()
+        self.add_sync_tf_frame('map', 'odom')
+        self.add_diff_drive_joint(parent_link_name='odom',
+                                  child_link_name='base_footprint',
+                                  odometry_topic='/tiago/base_footprint')
+        self.add_follow_joint_trajectory_server(namespace='/arm_left_controller/follow_joint_trajectory',
+                                                state_topic='/arm_left_controller/state')
+        self.add_follow_joint_trajectory_server(namespace='/arm_right_controller/follow_joint_trajectory',
+                                                state_topic='/arm_right_controller/state')
+        self.add_base_cmd_velocity(cmd_vel_topic='/tiago/cmd_vel',
+                                   track_only_velocity=False)
