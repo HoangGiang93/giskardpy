669c9be566217b62d955f690f0b13708d09b8086
Giang Nguyen <hoanggia@uni-bremen.de>
2023-04-14 16:18:50 +0200

    Fix tiago to tiago_dual

--- src/giskardpy/configs/tiago_unreal.py
+++ src/giskardpy/configs/tiago_unreal.py
@@ -25,10 +25,14 @@ class TiagoUnrealNoPrefix(Giskard):
         self.add_sync_tf_frame('map', 'odom')
         self.add_diff_drive_joint(parent_link_name='odom',
                                   child_link_name='base_footprint',
-                                  odometry_topic='/tiago/base_footprint')
+                                  odometry_topic='/tiago_dual/base_footprint')
         self.add_follow_joint_trajectory_server(namespace='/arm_left_controller/follow_joint_trajectory',
                                                 state_topic='/arm_left_controller/state')
         self.add_follow_joint_trajectory_server(namespace='/arm_right_controller/follow_joint_trajectory',
                                                 state_topic='/arm_right_controller/state')
-        self.add_base_cmd_velocity(cmd_vel_topic='/tiago/cmd_vel',
+        self.add_follow_joint_trajectory_server(namespace='/head_controller/follow_joint_trajectory',
+                                                state_topic='/head_controller/state')
+        self.add_follow_joint_trajectory_server(namespace='/torso_controller/follow_joint_trajectory',
+                                                state_topic='/torso_controller/state')
+        self.add_base_cmd_velocity(cmd_vel_topic='/tiago_dual/cmd_vel',
                                    track_only_velocity=False)
