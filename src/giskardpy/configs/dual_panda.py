7f570ae417fbc1effaebe2a0b6cbc51fd3f642fb
Giang Nguyen <hoanggia@uni-bremen.de>
2023-04-24 17:09:24 +0200

    Clean up dual_panda

--- src/giskardpy/configs/dual_panda.py
+++ src/giskardpy/configs/dual_panda.py
@@ -1,7 +1,7 @@
 from giskardpy.configs.default_giskard import Giskard
 
 
-class DualPanda(Giskard):
+class DualPanda1(Giskard):
     def __init__(self):
         self.add_robot_from_parameter_server(
             joint_state_topics=['/dual_panda/joint_states'])
@@ -9,5 +9,11 @@ class DualPanda(Giskard):
         self.add_fixed_joint('map', 'dual_panda/base')
         self.add_follow_joint_trajectory_server(namespace='/dual_panda/panda_1_joint_trajectory_controller/follow_joint_trajectory',
                                                 state_topic='/dual_panda/panda_1_joint_trajectory_controller/state')
+class DualPanda2(Giskard):
+    def __init__(self):
+        self.add_robot_from_parameter_server(
+            joint_state_topics=['/dual_panda/joint_states'])
+        super().__init__(root_link_name='map')
+        self.add_fixed_joint('map', 'dual_panda/base')
         self.add_follow_joint_trajectory_server(namespace='/dual_panda/panda_2_joint_trajectory_controller/follow_joint_trajectory',
                                                 state_topic='/dual_panda/panda_2_joint_trajectory_controller/state')
